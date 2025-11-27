7814f5e64603bebb638336f8dab0779fb6444204
priyanshuprafful <priyanshuprafful@gmail.com>
2025-11-27 17:34:32 +0530

    Add day 3

---
+++ Day-3/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+   }
+ }
+}
+
+# Configure the AWS Provider
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+
+
+# create s3 bucket
+resource "aws_s3_bucket" "my_bucket_one" {
+  bucket = "aws_terraform_prafful_bucket"
+  
+
+  tags = {
+    Name        = "TerraformExampleBucket"
+    Environment = "Dev"
+  }
+}
+# s3 bucket needs to be globally unique so append random string
