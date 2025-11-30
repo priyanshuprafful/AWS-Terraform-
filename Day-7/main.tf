a339a546a5d8ea38ff115031d9a4025c0179a48f
priyanshuprafful <priyanshuprafful@gmail.com>
2025-11-30 23:43:17 +0530

    message

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "example" {
   
   count = 1
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-026012a857b852da3" # RHEL 9 AMI in us-east-1
   instance_type = "t2.micro"
   region = var.region
 
