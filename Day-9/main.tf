6d1e696a707e46c126d430703c5d788956d7bbe8
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-05 02:27:55 +0530

    did some update or code change

--- Day-9/main.tf
+++ Day-9/main.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "example" {
   ami           = "ami-026012a857b852da3" # Example AMI ID, replace with a valid one for your region
   instance_type = var.instance_types[0]
-  region = var.region[1]
+  region = var.allowed_region[1]
 
   tags = var.tags
 } 
