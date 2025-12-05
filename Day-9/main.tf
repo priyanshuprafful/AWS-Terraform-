3ff7e1c77e8462f5090ad3e88961ba63eefc26d6
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-05 13:46:28 +0530

    did some update or code change

--- Day-9/main.tf
+++ Day-9/main.tf
@@ -8,6 +8,61 @@ resource "aws_instance" "example" {
 
   lifecycle {
     create_before_destroy = true 
-    prevent_destroy = true
+   # prevent_destroy = true
   }
 } 
+
+# ==============================
+# Example 3: ignore_changes
+# Use Case: Auto Scaling Group where capacity is managed externally
+# ==============================
+
+# Launch Template for Auto Scaling Group
+resource "aws_launch_template" "app_server" {
+  name_prefix   = "app-server-"
+  image_id      = "ami-026012a857b852da3"
+  instance_type = var.instance_types[0]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = var.tags
+    
+  }
+}
+
+# Auto Scaling Group
+resource "aws_autoscaling_group" "app_servers" {
+  name               = "app-servers-asg"
+  min_size           = 1
+  max_size           = 5
+  desired_capacity   = 2
+  health_check_type  = "EC2"
+  availability_zones = ["us-east-1a", "us-east-1b", "us-east-1c"]
+
+  launch_template {
+    id      = aws_launch_template.app_server.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "App Server ASG"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Demo"
+    value               = "ignore_changes"
+    propagate_at_launch = false
+  }
+
+  # Lifecycle Rule: Ignore changes to desired_capacity
+  # This is useful when auto-scaling policies or external systems modify capacity
+  # Terraform won't try to revert capacity changes made outside of Terraform
+  lifecycle {
+    ignore_changes = [
+      desired_capacity,
+      # Also ignore load_balancers if added later by other processes
+    ]
+  }
+}
