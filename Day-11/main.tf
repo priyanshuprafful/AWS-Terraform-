0c69017130d1418382fe513559553c1473bfc0ab
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-16 22:48:15 +0530

    did some update or code change

--- Day-11/main.tf
+++ Day-11/main.tf
@@ -2,7 +2,3 @@ locals {
     formatted_project_name = lower(var.project_name)
 }
 
-output "formatted_project_name" {
-    value = local.formatted_project_name
-  
-}
--- Day-11/outputs.tf
+++ Day-11/outputs.tf
@@ -1,4 +1,4 @@
-# output "vpc_id" {
-#   value = aws_vpc.my_vpc.id
+output "formatted_project_name" {
+    value = local.formatted_project_name
   
-# }
+}
