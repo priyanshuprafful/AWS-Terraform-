7d27395c88cf9f1901ea67dbb43ac4cc32259596
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-17 14:32:11 +0530

    did some update or code change

--- Day-11/main.tf
+++ Day-11/main.tf
@@ -1,7 +1,7 @@
 locals {
     formatted_project_name = lower(var.project_name)
     formatted_bucket_name = replace(replace(substr(lower(var.bucket_name), 0, 63)," ", ""), "!", "")
-    port_list = split(",", var.allowed_ports)
+    port_list = tonumber(split(",", var.allowed_ports))
 
     sg_rules = [
         for port in local.port_list : {
