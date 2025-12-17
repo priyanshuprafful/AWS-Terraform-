152e2b233933df69384e232b98b42cbd85dd0d58
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-17 12:08:38 +0530

    did some update or code change

--- Day-11/outputs.tf
+++ Day-11/outputs.tf
@@ -2,3 +2,7 @@ output "formatted_project_name" {
     value = local.formatted_project_name
   
 }
+
+output "formatted_project_name_repo" {
+    value = upper(replace(local.formatted_project_name, "_", "-"))
+}  
