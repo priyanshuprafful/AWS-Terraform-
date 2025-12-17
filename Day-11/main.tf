c6afee542fc28464b861ca042302a8e32d14b581
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-17 12:44:14 +0530

    did some update or code change

--- Day-11/main.tf
+++ Day-11/main.tf
@@ -2,3 +2,11 @@ locals {
     formatted_project_name = lower(var.project_name)
 }
 
+# create s3 bucket
+resource "aws_s3_bucket" "project_bucket_day11" {
+    bucket = replace(local.formatted_project_name, " ", "-")
+    
+    tags = {
+        Environment = "dev"
+    }   
+}  
