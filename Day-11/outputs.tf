4c372ea450e4293898587bc35e8b58c2f211b9ba
priyanshuprafful <priyanshuprafful@gmail.com>
2025-12-17 13:02:33 +0530

    did some update or code change

--- Day-11/outputs.tf
+++ Day-11/outputs.tf
@@ -6,3 +6,8 @@ output "formatted_project_name" {
 output "formatted_project_name_repo" {
     value = upper(replace(local.formatted_project_name, " ", "-"))
 }  
+
+output "s3_bucket_name" {
+    value = aws_s3_bucket.project_bucket_day11.bucket
+  
+}
